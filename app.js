c88610bbd4f17e0482d895b8de01ffdfcec2bd66
UmidjonShamsutdinov <umid.2825@gmail.com>
2023-09-17 15:21:24 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,181 @@
+const mainSwiper = document.querySelector(".swiper-wrapper")
+const firstLineSwiper = document.querySelector(".swiper-wrapper1")
+const secondLineSwiper = document.querySelector(".swiper-wrapper2")
+const thirdLineSwiper = document.querySelector(".swiper-wrapper3")
+const fourthLineSwiper = document.querySelector(".swiper-wrapper4")
+const fifthLineSwiper = document.querySelector(".swiper-wrapper5")
+const bannerContent = document.querySelector(".hero__content")
+
+
+
+
+const options = {
+    method : "GET",
+    headers : {
+        accept : "application/json",
+        Authorization : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjhjZjc4ZDdlMjRkZjIxNDIyNGM1ZTIzOGJmNDk4OSIsInN1YiI6IjY1MDEzNGI0ZWZlYTdhMDBjMzk3MWFkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.py2h2aWnyIx_Abbvzzb5HIaLXmz5CeVkpWAbhyKPPlQ"
+    }
+}
+
+fetch("https://api.themoviedb.org/3/movie/top_rated?&with_networks=213", options)
+.then(response => response.json())
+.then(data => heroRender(data))
+
+
+function heroRender(data){
+    const fragment = document.createDocumentFragment()
+    data.results.forEach(e => {
+        const div = document.createElement("div")
+        const heroContent = document.createElement("div")
+        heroContent.className = "hero__content"
+        heroContent.innerHTML = `
+                <h2 class="hero__title">${e.title}</h2>
+                <div class="hero__content-btns">
+                    <button><i class="bi bi-play-fill"></i>play</button>
+                    <button>More Info</button>
+                </div>
+        `
+        div.className = "swiper-slide"
+        const img = document.createElement("img")
+        img.src = "https://image.tmdb.org/t/p/original/" + e.backdrop_path
+        div.appendChild(img)
+        div.appendChild(heroContent)
+        fragment.appendChild(div)
+    })
+
+    mainSwiper.appendChild(fragment)
+}
+
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/discover/tv", options)
+.then(response => response.json())
+.then(date => innerRender(date))
+
+function innerRender(date){
+    const innerFragment = document.createDocumentFragment();
+    date.results.forEach(e => {
+        // console.log(e);
+        const elementId = e.id
+        const div = document.createElement("div")
+        div.className = "swiper-slide1"
+        const imgLink = "https://image.tmdb.org/t/p/original/"
+        div.innerHTML = `
+        <a href="${"./movie_info/index.html?infoId="+elementId}"><img src="${imgLink + e.poster_path}" alt="image"/></a>
+        <p class="title" title="${e.name}">${e.name.split("").length>10 ? e.name.slice(0, 11) + "..." : e.name}</p>
+        `
+        innerFragment.appendChild(div)        
+    })
+    firstLineSwiper.appendChild(innerFragment)
+}
+
+
+fetch("https://api.themoviedb.org/3/discover/movie?&with_genres=28", options)
+.then(response => response.json())
+.then(data => otherLinesRender(data))
+
+
+
+
+
+function otherLinesRender(date){
+    const innerFragment = document.createDocumentFragment();
+    date.results.forEach(e => {
+        const elementId = e.id
+        const div = document.createElement("div")
+        div.className = "swiper-slide2"
+        const imgLink = "https://image.tmdb.org/t/p/original/"
+        div.innerHTML = `
+        <a href="${"./movie_info/index.html?infoId="+elementId}"><img src="${imgLink + e.backdrop_path}" alt="image"/></a>
+        <p class="title" title="${e.original_title}">${e.original_title.split("").length>20 ? e.original_title.slice(0, 21) + "..." : e.original_title}</p>
+        `
+        innerFragment.appendChild(div)
+        
+    })
+    secondLineSwiper.appendChild(innerFragment)
+}
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/discover/movie?&with_genres=12", options)
+.then(response => response.json())
+.then(data => otherLinesRender2(data))
+
+
+function otherLinesRender2(date){
+    const innerFragment = document.createDocumentFragment();
+    date.results.forEach(e => {
+        const elementId = e.id
+        const div = document.createElement("div")
+        div.className = "swiper-slide3"
+        const imgLink = "https://image.tmdb.org/t/p/original/"
+        div.innerHTML = `
+        <a href="${"./movie_info/index.html?infoId="+elementId}"><img src="${imgLink + e.backdrop_path}" alt="image"/></a>
+        <p class="title" title="${e.original_title}">${e.original_title.split("").length>20 ? e.original_title.slice(0, 21) + "..." : e.original_title}</p>
+        `
+        innerFragment.appendChild(div)
+        
+    })
+    thirdLineSwiper.appendChild(innerFragment)
+}
+
+
+
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/discover/movie?&with_genres=35", options)
+.then(response => response.json())
+.then(data => otherLinesRender4(data))
+
+
+function otherLinesRender4(date){
+    const innerFragment = document.createDocumentFragment();
+    date.results.forEach(e => {
+        const elementId = e.id
+        const div = document.createElement("div")
+        div.className = "swiper-slide4"
+        const imgLink = "https://image.tmdb.org/t/p/original/"
+        div.innerHTML = `
+        <a href="${"./movie_info/index.html?infoId="+elementId}"><img src="${imgLink + e.backdrop_path}" alt="image"/></a>
+        <p class="title" title="${e.original_title}">${e.original_title.split("").length>20 ? e.original_title.slice(0, 21) + "..." : e.original_title}</p>
+        `
+        innerFragment.appendChild(div)
+        
+    })
+    fourthLineSwiper.appendChild(innerFragment)
+}
+
+
+
+
+
+
+fetch("https://api.themoviedb.org/3/discover/movie?&with_genres=16", options)
+.then(response => response.json())
+.then(data => otherLinesRender5(data))
+
+
+function otherLinesRender5(date){
+    const innerFragment = document.createDocumentFragment();
+    date.results.forEach(e => {
+        const elementId = e.id
+        const div = document.createElement("div")
+        div.className = "swiper-slide5"
+        const imgLink = "https://image.tmdb.org/t/p/original/"
+        div.innerHTML = `
+        <a href="${"./movie_info/index.html?infoId="+elementId}"><img src="${imgLink + e.backdrop_path}" alt="image"/></a>
+        <p class="title" title="${e.original_title}">${e.original_title.split("").length>20 ? e.original_title.slice(0, 21) + "..." : e.original_title}</p>
+        `
+        innerFragment.appendChild(div)
+        
+    })
+    fifthLineSwiper.appendChild(innerFragment)
+}
